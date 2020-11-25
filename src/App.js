4472c53d341776404f83c99784127e64357a8690
mekedem <mekedemgetaneh@gmail.com>
2020-11-25 22:07:32 +0300

    added more routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Signup from "./components/signuppage/signuppage";
+import Login from "./components/loginpage/loginpage";
+import ForgotPassword from "./components/forgotpassword/forgotpassword";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route
-          path="/"
-          name="BayerCreate"
+          path="/signup"
+          name="signup"
           component={(props) => <Signup {...props} />}
+        />
+        <Route
+          path="/login"
+          name="signin"
+          component={(props) => <Login {...props} />}
+        />
+        <Route
+          path="/forgotpassword"
+          name="forgotpassword"
+          component={(props) => <ForgotPassword {...props} />}
+        />
+        <Route
+          path="/adminpanel"
+          name="adminpanel"
+          component={(props) => <AdminPanel {...props} />}
         />
       </Switch>
     </BrowserRouter>
