7c7e3ff123dedf913851ec9b845a8f3ff5b7354b
mekedem <mekedemgetaneh@gmail.com>
2020-11-27 16:46:04 +0300

    signin saga is added

--- src/saga/authSaga.js
+++ src/saga/authSaga.js
@@ -1,18 +1,39 @@
 import { put, takeLatest, takeEvery, call } from "redux-saga/effects";
-import { signupuser } from "../API/authAPI";
+import { signupuser, signinuser } from "../API/authAPI";
+import { signupResponse, signupError, signinResponse, signinError } from "../actions/authActions";
+import "../constants/constants";
+import { LOGIN_REQUEST, SIGNUP_REQUEST } from "../constants/constants";
 
 const signupRequestSaga = function* (action) {
     const userCredential = action.payload;
     try {
         const response = yield call(signupuser, userCredential);
         if (response.success) {
-            console.log("yemetaw keserver nw : ", response.data);
-        } else console.log("signup error has occured");
+            yield put(signupResponse(response.data));
+            // userCredential.history.push("/projectForm");
+        } else yield put(signupError(response.message));
+    } catch (error) {
+        console.log("Connection Error happened", error);
+    }
+};
+
+const signinRequestSaga = function* (action) {
+    const userCredential = action.payload;
+    try {
+        const response = yield call(signinuser, userCredential);
+        if (response.success) {
+            yield put(signinResponse(response.data));
+            // userCredential.history.push("/projectForm");
+        } else yield put(signinError(response.message));
     } catch (error) {
         console.log("Connection Error happened", error);
     }
 };
 
 export const watchSignupRequest = function* () {
-    yield takeLatest('SIGNUP_REQUEST', signupRequestSaga);
+    yield takeLatest(SIGNUP_REQUEST, signupRequestSaga);
+};
+
+export const watchSigninRequest = function* () {
+    yield takeLatest(LOGIN_REQUEST, signinRequestSaga);
 };
