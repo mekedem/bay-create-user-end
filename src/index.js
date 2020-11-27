8804d4970b9db2ae6c59c557c9075e36c99b2914
mekedem <mekedemgetaneh@gmail.com>
2020-11-27 12:16:07 +0300

    added saga and redux store config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
+import configureStore from './store/configurestore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
