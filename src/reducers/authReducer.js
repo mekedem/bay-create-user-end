431e4fd1638fa1f9b262ec17c9f3f05799bb2fda
mekedem <mekedemgetaneh@gmail.com>
2020-11-27 16:45:28 +0300

    signup and login reducers are added here

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,37 @@
+import "../constants/constants";
+import { LOGIN_FAILURE, LOGIN_SUCCESS, SIGNUP_FAILURE, SIGNUP_SUCCESS, USER_TOKEN } from "../constants/constants";
+
 const initialState = {
     userInfo: [],
+    ErrorMessage: ""
 };
 
 const authenticationReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'SIGNUP_REQUEST':
-            console.log("signup requested at reducer");
-            return state;
+        case SIGNUP_SUCCESS:
+            return {
+                userInfo: action.payload.data,
+                ErrorMessage: "",
+            };
+
+        case SIGNUP_FAILURE:
+            return {
+                userInfo: state.userInfo,
+                ErrorMessage: action.payload.message,
+            };
+
+        case LOGIN_SUCCESS:
+            localStorage.setItem(USER_TOKEN, action.payload.data.token);
+            return {
+                userInfo: action.payload.data,
+                ErrorMessage: "",
+            };
+
+        case LOGIN_FAILURE:
+            return {
+                userInfo: state.userInfo,
+                ErrorMessage: action.payload.message,
+            };
         default:
             return state;
     }
