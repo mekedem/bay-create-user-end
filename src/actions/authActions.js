a9ff36ddb7747284856df72faecf885448cb5dce
mekedem <mekedemgetaneh@gmail.com>
2020-11-27 16:43:16 +0300

    authentication redux actions are implemented signin and signup

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,10 +1,52 @@
+import "../constants/constants"
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, SIGNUP_FAILURE, SIGNUP_REQUEST, SIGNUP_SUCCESS } from "../constants/constants";
+
 export const signupRequested = ({ email, fullname, password, affiliationcode, phonenumber }) => ({
-    type: 'SIGNUP_REQUEST',
+    type: SIGNUP_REQUEST,
     payload: {
         email: email,
         fullName: fullname,
         password: password,
         affiliationCode: affiliationcode,
         phone: phonenumber
+    },
+});
+
+
+export const signupResponse = (data) => ({
+    type: SIGNUP_SUCCESS,
+    payload: {
+        data,
+    },
+});
+
+export const signupError = (message) => ({
+    type: SIGNUP_FAILURE,
+    payload: {
+        message,
+    },
+});
+
+
+export const signinRequested = ({ email, password }) => ({
+    type: LOGIN_REQUEST,
+    payload: {
+        email: email,
+        password: password
+    },
+});
+
+
+export const signinResponse = (data) => ({
+    type: LOGIN_SUCCESS,
+    payload: {
+        data,
+    },
+});
+
+export const signinError = (message) => ({
+    type: LOGIN_FAILURE,
+    payload: {
+        message,
     },
 });
