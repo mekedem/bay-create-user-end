1f525b24499526ea60f77ecf776ad8bad76d09eb
mekedem <mekedemgetaneh@gmail.com>
2020-11-27 16:46:45 +0300

    minor change

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,8 +1,9 @@
 import { all } from "redux-saga/effects";
-import { watchSignupRequest } from "./authSaga";
+import { watchSignupRequest, watchSigninRequest } from "./authSaga";
 
 export default function* rootSaga() {
     yield all([
         watchSignupRequest(),
+        watchSigninRequest()
     ]);
 }
