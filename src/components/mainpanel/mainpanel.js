7311d81ac63b544bdb6aa044fba9637c149183e1
mekedem <mekedemgetaneh@gmail.com>
2020-11-28 09:17:34 +0300

    passed props

--- src/components/mainpanel/mainpanel.js
+++ src/components/mainpanel/mainpanel.js
@@ -24,7 +24,7 @@ export default function mainPanel(props) {
                         <UserManagement />
                     </Route>
                     <Route path="/servicerequest">
-                        <ServiceRequest props={props} />
+                        <ServiceRequest {...props} />
                     </Route>
                 </Switch>
             </div>
