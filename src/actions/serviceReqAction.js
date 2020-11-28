ae332d72ced3ca5c6abf1e0bfa237e99d0f87a58
mekedem <mekedemgetaneh@gmail.com>
2020-11-28 09:17:15 +0300

    added functionality to service request action

--- src/actions/serviceReqAction.js
+++ src/actions/serviceReqAction.js
@@ -1,8 +1,22 @@
-import { GET_SERVICE_REQUEST } from "../constants/constants";
+import { GET_SERVICE_REQUEST, SERVICE_REQUEST_RESPONSE, SERVICE_REQUEST_ERROR } from "../constants/constants";
 
 export const getServiceRequest = ({ pagevalue }) => ({
     type: GET_SERVICE_REQUEST,
     payload: {
         pagevalue: pagevalue,
+    },
+});
+
+export const serviceResponse = (data) => ({
+    type: SERVICE_REQUEST_RESPONSE,
+    payload: {
+        data,
+    },
+});
+
+export const serviceResponseError = (message) => ({
+    type: SERVICE_REQUEST_ERROR,
+    payload: {
+        message,
     },
 });
