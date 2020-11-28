93a298cb2f04e867a82214036dd902370898e8f5
mekedem <mekedemgetaneh@gmail.com>
2020-11-28 09:18:21 +0300

    minor changes

--- src/components/mainpanel/usermanagement/usermanagement.js
+++ src/components/mainpanel/usermanagement/usermanagement.js
@@ -5,20 +5,7 @@ const UserManagement = () => {
 
     return (<>
         <div>
-            <table>
-                <tbody>
-                    <tr className="department-item-container">
-                        <td className="department-item-name"><button className="buttonadd">+</button></td>
-                        <td className="department-item-description"><h2>User Info</h2></td>
-                    </tr>
-                    <tr className="department-item-container">
-                        <td className="department-item-name">UserName</td>
-                        <td className="department-item-name">Mobile</td>
-                        <td className="department-item-name">Email</td>
-                        <td className="department-item-member">buttons here</td>
-                    </tr>
-                </tbody>
-            </table>
+            centered at user management here...
         </div>
     </>
     );
