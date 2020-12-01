00db69cbaf854a3ff81e5ecf8cd61ce9a2169790
mekedem <mekedemgetaneh@gmail.com>
2020-12-01 17:05:05 +0300

    added requester function for service and validation for the input

--- src/utilcomponents/requestservicemodal/requestservicemodal.js
+++ src/utilcomponents/requestservicemodal/requestservicemodal.js
@@ -1,29 +1,80 @@
-import React, { useState } from "react";
+import React from "react";
 import "./servicemodal.scss";
 import Modal from "../modals/modal";
+import * as Yup from "yup";
+import { Form, Field, Formik } from "formik";
+import { requireService } from "../../API/serviceRequestAPI";
 
-const requestServiceModal = () => {
+
+const requestSchema = () => {
+    return Yup.object().shape({
+        description: Yup.string()
+            .min(10, "Too Short!")
+            .max(150, "Too Long!")
+            .required("* Required"),
+    });
+};
+
+
+
+const requestServiceModal = ({ handleClose }) => {
+
+    const onSubmit = (values) => {
+        setTimeout(() => {
+            requireService({ description: values.description, files: [] })
+                .then((response) => {
+                    console.log(response)
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                .then(() => { });
+        }, 600);
+    };
 
     return (
         <Modal show={true}>
             <div className="modal-title">Add Request</div>
             <div className="modal-body">
-                <form>
-                    <div className="modal-footer">
-                        <button
-                            variant="outlined"
-                            size="l"
-                            onClick={() => { }}>
-                            Cancel
+                <Formik
+                    initialValues={{
+                        description: ""
+                    }}
+                    validationSchema={requestSchema()}
+                    onSubmit={onSubmit}
+                >
+                    {({ errors, touched }) => (
+                        <Form>
+                            <div className="input-lable">
+                                <label>Description </label>
+                                <Field component="textarea" className="input-text" name="description" cols="45" rows="3" />
+                                {touched.description && errors.description && <p className="signinformerror">{errors.description}</p>}
+                            </div>
+                            <div className="input-lable">
+                                <label>Status </label>
+                                <Field className="requestStatus" component="select" name="status" placeholder="requesting">
+                                    <option key="b" value="a">Initial Request</option>
+                                </Field>
+                            </div>
+                            <div className="input-lable">
+                                <label>Attach</label>
+                                <input type="file" className="fileuploadinput" />
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    className="modalcancelbtn"
+                                    onClick={() => handleClose()}>
+                                    Cancel
                 </button>
-                        <button
-                            variant="primary"
-                            size="l"
-                            type="submit">
-                            Submit Request
+                                <button
+                                    className="modalsubmitbtn"
+                                    type="submit">
+                                    Submit Request
                 </button>
-                    </div>
-                </form>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </div>
         </Modal>
     );
