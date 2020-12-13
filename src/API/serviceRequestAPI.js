c0147a51a1411d466172aabf1084b34ef9d768e8
mekedem <mekedemgetaneh@gmail.com>
2020-12-13 17:02:18 +0300

    getDownloadFile added and save prepared

--- src/API/serviceRequestAPI.js
+++ src/API/serviceRequestAPI.js
@@ -20,8 +20,29 @@ export const getInitialData = () => {
     return axios.get(url, { headers: { Authorization: token, something: 'this is' } }).then(d => d.data);
 }
 
+export const getDownloadFile = async (fileurl) => {
+    return axios.get(`${baseURL}${fileurl}`, {
+        responseType: 'blob',
+    }).then(response => response.blob())
+}
+
 export const requireService = (request) => {
     const token = localStorage.getItem(USER_TOKEN);
     const url = baseURL + `/${GETSERVICEREQUEST_URL}`;
-    return axios.post(url, request, { headers: { Authorization: token } }).then(d => d.data);
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('description', request.description);
+    bodyFormData.append('files', request.files);
+    return axios({
+        method: 'post',
+        url: url,
+        data: bodyFormData,
+        headers: { 'Content-Type': 'multipart/form-data', Authorization: token }
+    })
+        .then(d => d.data)
+        .catch(function (response) {
+            console.log(response);
+        });
+
+    // return axios.post(url, request, { headers: { Authorization: token } }).then(d => d.data);
 }
